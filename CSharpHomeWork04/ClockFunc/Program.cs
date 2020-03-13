97137dd298d3bdbc5ca90aa2c88380703504c2fe
sdProgrammer0614 <2018302141022@whu.edu.cn>
2020-03-13 18:16:28 +0800

    CSharp HomeWork for 3.13

---
+++ CSharpHomeWork04/ClockFunc/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace ClockFunc
+{
+    // 声明一个委托类型，定义事件处理函数的格式
+    public delegate void ClockHandler(object sender, DateTime dateTime);
+
+    public class Clock
+    {
+        // 定义事件，相当于创建一个委托实例
+        public event ClockHandler clockHandler;
+        private DateTime clockTime;     // 闹钟响铃时间
+
+        public Clock(DateTime clockTime)
+        {
+            this.clockTime = clockTime;
+        }
+
+        public void Start()     // 启动闹钟
+        {
+            while(true)
+            {
+                clockHandler(this, clockTime);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public class ClockSimulation
+    {
+        public Clock clock;
+        public ClockSimulation(DateTime dateTime)
+        {
+            clock = new Clock(dateTime);
+            clock.clockHandler += Tick;
+            clock.clockHandler += Alarm;
+        }
+
+        public void Tick(object sender, DateTime dateTime)
+        {
+            if (!(DateTime.Now.Hour == dateTime.Hour && DateTime.Now.Minute == dateTime.Minute))
+                Console.WriteLine("tick");
+        }
+
+        public void Alarm(object sender, DateTime dateTime)
+        {
+            if (DateTime.Now.Hour == dateTime.Hour && DateTime.Now.Minute == dateTime.Minute)
+                Console.WriteLine("ring");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockSimulation simulateClock = 
+                new ClockSimulation(new DateTime(2020, 3, 13, 18, 00, 00));     // 设置闹钟响铃时间
+            simulateClock.clock.Start();
+        }
+    }
+}
---
+++ CSharpHomeWork04/Node/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            } else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> node = head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intList.Add(x);
+            }
+
+            // 打印链表元素
+            intList.ForEach(m => Console.WriteLine(m));
+
+            // 求最大值、最小值
+            int max = intList.Head.Data;
+            int min = intList.Head.Data;
+            intList.ForEach(m => max = max < m ? m : max);
+            intList.ForEach(m => min = min > m ? m : min);
+            Console.WriteLine($"Max = {max}, Min = {min}");
+
+            // 求和
+            int sum = 0;
+            intList.ForEach(m => sum += m);
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
